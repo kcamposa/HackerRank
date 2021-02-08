1debc961c62c137699ce8b7d7d808f44645130c6
Kevin Campos Alfaro <kcamposa@gruposur.com>
2021-02-08 16:34:33 -0600

    'count +' and 'count -' at the same time

--- printStairs.py
+++ printStairs.py
@@ -1,11 +1,15 @@
 def printStairs(n):
     countUp = 1
-    countDown = n
+    countDown = n-1
 
-    while(countUp <= n):
-        
-        print((" " * countDown) + ("#" * countUp))
-        countUp += 1
-        countDown += -1
+    while(countUp <= n): 
+        if countUp == n:
+            print(("#" * countUp))
+            countUp += 1
+            countDown += -1
+        else:
+            print((" " * countDown) + ("#" * countUp))
+            countUp += 1
+            countDown += -1
 
 printStairs(6)
