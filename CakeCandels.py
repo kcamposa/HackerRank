48a0ba3d825d5dc7b307482d353380431762bc20
Kevin Campos Alfaro <kev_day09@hotmail.es>
2021-02-09 10:33:56 -0600

    Finished

--- CakeCandels.py
+++ CakeCandels.py
@@ -4,6 +4,7 @@
 # Count how many candles are tallest.
 # example:  candles = [3, 2, 1, 3]
 # The maximum height candles are 4 units high. There are 2 of them, so return 2.
+#
 
 def CakeCandles():
 
