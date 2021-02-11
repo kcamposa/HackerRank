77412e8937d0f77d9c1a64c91ad9bc8b5018b24a
Kevin Campos Alfaro <kcamposa@gruposur.com>
2021-02-11 16:22:57 -0600

    almost completed

--- NumberLineJumpsKangoroo.py
+++ NumberLineJumpsKangoroo.py
@@ -40,7 +40,7 @@ def NumberLineJumps(x1, v1, x2, v2):
 
 
 
-def NumberLineJumps_1(x1, v1, x2, v2):
+def NumberLineJumps_1(x1, v1, x2, v2): # completed
 
     print("YES" if (x2 - x1) * (v2 - v1) < 0 and (x2 - x1) % (v2 - v1) == 0 else "NO")
 
