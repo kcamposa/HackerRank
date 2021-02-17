e68fec6c37bd4dc65ccdff0cc27434d04f868cf4
unknown <kev_day09@hotmail.es>
2021-02-16 23:27:36 -0600

    to do

---
+++ SubarrayDivision.py
@@ -0,0 +1,4 @@
+# Given a chocolate bar, two children, Lily and Ron, are determining how to share it. Each of the squares has an integer on it.
+# Lily decides to share a contiguous segment of the bar selected such that:
+# -The length of the segment matches Ron's birth month.
+# -The sum of the integers on the squares is equal to his birth day.
