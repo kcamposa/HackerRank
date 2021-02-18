6b10d6e29c38bcb52481a83c6e47ab9aadd443e9
Kevin Campos Alfaro <kcamposa@gruposur.com>
2021-02-18 11:33:32 -0600

    added same amount, only an amount, ...

--- 14_MigratoryBirds.py
+++ 14_MigratoryBirds.py
@@ -1,11 +1,10 @@
 
-birds = [1, 4, 4, 4, 5, 3] # 1 3 4 4 4 5
-#birds = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
-newList = []
+#birds = [1, 4, 4, 4, 5, 3] # 1 3 4 4 4 5
+birds = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
 
 def migratoryBirds_1(arr):
 
-    
+    newList = []
 
     for i in range(1, 6):
         newList.append(arr.count(i))
@@ -15,17 +14,14 @@ def migratoryBirds_1(arr):
     max_ = max(newList)
 
     if arr.count(max_)>1:
-        pass
-    else:
-        index = SearchMax(max_)
-        print(index)
-
-def SearchMax(number):
-
-    for i in range(5):
-        num = newList[i]
-        if newList[i] == number:
-            return i+1
+        if arr.count(max_)==5: # numbers same amount
+            print(arr[0])
+        else:
+            pass
+    else: # only max 1
+        for i in range(5):
+            if newList[i] == max_:
+                print(i+1)
 
     
 
