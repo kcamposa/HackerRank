8266458747a8e94b640293221c1330ddcf848856
Kevin Campos Alfaro <kcamposa@gruposur.com>
2021-02-18 11:02:05 -0600

    to do

--- 14_MigratoryBirds.py
+++ 14_MigratoryBirds.py
@@ -1,29 +1,17 @@
 
 birds = [1, 4, 4, 4, 5, 3] # 1 3 4 4 4 5
+#birds = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
 
+def migratoryBirds_1(arr):
 
-def migratoryBirds(arr):
+    num = 0
+    newList = []
+
+    for i in range(1, 6):
+        newList.append(arr.count(i))
     
-    NewList = []
-    newBirds = birds
+
     
+    print(newList)
 
-
-    #for j in newBirds:
-    #    if newBirds.count(j)>1:
-    #        newBirds.remove(j)
-
-    #print(newBirds)
-
-    #for position in range(0, len(newBirds)):
-    for position in range(0, len(arr)):
-        count = 0
-        for j in arr:
-            if j in arr:
-                count += 1
-
-        NewList.insert(arr[position], count)
-        
-    print(NewList)
-
-migratoryBirds(birds)
+migratoryBirds_1(birds)
