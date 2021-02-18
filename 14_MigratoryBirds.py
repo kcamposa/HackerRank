f743be37d77b1d08eef42e6869997e3aa33c783b
unknown <kev_day09@hotmail.es>
2021-02-17 21:00:16 -0600

    to do

--- 14_MigratoryBirds.py
+++ 14_MigratoryBirds.py
@@ -2,15 +2,27 @@
 birds = [1, 4, 4, 4, 5, 3] # 1 3 4 4 4 5
 
 
-def migratoryBirds():
-    birds1 = [1, 4, 4, 4, 5, 3] # 1 3 4 4 4 5
-    return len(birds1)
+def migratoryBirds(arr):
+    
+    NewList = []
+    newBirds = birds
+    
 
 
-x = map(migratoryBirds, (1, 4, 4, 4, 5, 3))
+    for j in newBirds:
+        if newBirds.count(j)>1:
+            newBirds.remove(j)
 
-print(list(x))
+    print(newBirds)
 
+    for position in range(0, len(newBirds)):
+        count = 0
+        for j in arr:
+            if j in arr:
+                count += 1
 
+        NewList.insert(arr[position], count)
+        
+    print(NewList)
 
-#migratoryBirds(birds)
+migratoryBirds(birds)
