7805835f9d60afa781cee96e2141b5c71d9c4f83
Kevin Campos Alfaro <kcamposa@gruposur.com>
2021-02-18 14:10:24 -0600

    Finished

--- 14_MigratoryBirds.py
+++ 14_MigratoryBirds.py
@@ -1,3 +1,7 @@
+# Given an array of bird sightings where every element represents a bird type id, determine the id # of the most frequently sighted type. If more than 1 type has been spotted that maximum amount, # # return the smallest of their ids.
+
+
+
 
 #birds = [1, 4, 4, 4, 5, 3] # 1 3 4 4 4 5
 birds = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
