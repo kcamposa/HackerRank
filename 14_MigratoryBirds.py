cd2d40c513b4534f7a3591c4187a8ba7f9d009c7
unknown <kev_day09@hotmail.es>
2021-02-17 21:05:17 -0600

    to do

--- 14_MigratoryBirds.py
+++ 14_MigratoryBirds.py
@@ -9,13 +9,14 @@ def migratoryBirds(arr):
     
 
 
-    for j in newBirds:
-        if newBirds.count(j)>1:
-            newBirds.remove(j)
+    #for j in newBirds:
+    #    if newBirds.count(j)>1:
+    #        newBirds.remove(j)
 
-    print(newBirds)
+    #print(newBirds)
 
-    for position in range(0, len(newBirds)):
+    #for position in range(0, len(newBirds)):
+    for position in range(0, len(arr)):
         count = 0
         for j in arr:
             if j in arr:
