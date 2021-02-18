0b355ec944836e9b2a42a216b16c222e39591acf
Kevin Campos Alfaro <kcamposa@gruposur.com>
2021-02-18 11:16:17 -0600

    to do

--- 14_MigratoryBirds.py
+++ 14_MigratoryBirds.py
@@ -1,17 +1,32 @@
 
 birds = [1, 4, 4, 4, 5, 3] # 1 3 4 4 4 5
 #birds = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
+newList = []
 
 def migratoryBirds_1(arr):
 
-    num = 0
-    newList = []
+    
 
     for i in range(1, 6):
         newList.append(arr.count(i))
+
+    print(newList)
     
+    max_ = max(newList)
+
+    if arr.count(max_)>1:
+        pass
+    else:
+        index = SearchMax(max_)
+        print(index)
+
+def SearchMax(number):
+
+    for i in range(5):
+        num = newList[i]
+        if newList[i] == number:
+            return i+1
 
     
-    print(newList)
 
 migratoryBirds_1(birds)
