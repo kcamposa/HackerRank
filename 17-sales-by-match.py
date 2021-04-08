3fcac99de72c5a6f6542af81bcb8a78cff2d287a
kcamposa <kev_day09@hotmail.es>
2021-04-08 17:15:11 -0600

    finished

--- 17-sales-by-match.py
+++ 17-sales-by-match.py
@@ -1,26 +1,24 @@
 '''
 There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
-
-Example
+Example:
 n = 7
 ar = [1, 2, 1, 2, 1, 3, 2]
 
 There is one pair of color 1 and one of color 2. There are three odd socks left, one of each color. The number of pairs is 2.
-
 Complete the sockMerchant function in the editor below.
-
 - int n: the number of socks in the pile
 - int ar[n]: the colors of each sock
 
-returns int: the number of pairs
+**returns int: the number of pairs
 '''
 
 n = 7
-#ar = [1, 2, 1, 2, 1, 3, 2]
-ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+ar = [1, 2, 1, 2, 1, 3, 2]
+#ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 
 def sockMerchant(n, ar):
 
+    i = 0
     numbers = []
     countsNumbers = []
     nsPairs = []
@@ -28,7 +26,6 @@ def sockMerchant(n, ar):
     #print(numbers)
 
     for ns in numbers:
-        #num = ar.count(ns)
         countsNumbers.append(ar.count(ns))
 
     #print(countsNumbers) # [4, 3, 1, 1]
@@ -41,6 +38,12 @@ def sockMerchant(n, ar):
             if nsP%2 == 0:
                 nsPairs.append(nsP)
 
-    print(nsPairs) # [4, 2]
+    #print(nsPairs) # [4, 2]
+
+    for mun in nsPairs:
+        num = mun // 2
+        i = i + num
+
+    print(i)
 
 sockMerchant(n, ar)
