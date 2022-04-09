f5d246b7d9d676a3de3db150cb0aa9ebe1c2c760
Kevin Campos Alfaro <kevincamposalfarogmail.com>
2022-04-09 09:56:50 -0600

    finished

--- 28-designerPdfViewer.py
+++ 28-designerPdfViewer.py
@@ -1,4 +1,5 @@
 ''''
+
 There is a list of 26 character heights aligned by index to their letters. For example, "a" is at index 0 and "z" is at index 25. There will also be string.
 Using the letter heights given, determine the area of the rectangle highlight in mm**2 assuming all letters are 1mm wide.
 
@@ -12,15 +13,13 @@ The heights are t=2. o=1, r=1 and n=1. The tallest letter is 2 high and there ar
 
 '''
 
-
 #     a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
 #h = [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 #word = "abc"
 
 #    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
 h = [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7]
-word = "zaba"
-# output 28
+word = "zaba" # output 28
 
 def designerPdfViewer(h, word):
 
